2b3239898c56ee4e973e2d2e4dd0bb12c571fed4
kamillamamatova <kamillamamatova.km@gmail.com>
2025-01-26 14:52:16 -0500

    Create calendar.c

---
+++ calendar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+  int days, start;
+
+  // Reads in days in calendar.
+  printf("Each number of days in month: ");
+  scanf("%d", days);
+
+  // Reads in start day.
+  printf("\nEnter starting day of the week (1-Sun, 7-Sat): ");
+  scanf("%d", start);
+
+  printf("\n");
+
+  
+  
